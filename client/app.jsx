944763ae9c4e5e935004719874e157072a6f5fd5
Jeff Kwon <jykwon007@gmail.com>
2022-07-15 02:56:16 +0000

    Header, week view added.

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import Home from './pages/home';
+import React/* { useState, useEffect } */from 'react';
+import Week from './pages/day';
+import Header from './components/header';
+// import { parseRoute } from './lib';
 
-export default class App extends React.Component {
-  render() {
-    return <Home />;
-  }
-}
+// export default class App extends React.Component {
+//   render() {
+//     return <Home />;
+//   }
+// }
+
+const App = () => {
+  // const [route, setRoute] = useState(parseRoute(window.location.hash));
+
+  // useEffect(() => {
+  //   window.addEventListener('hashchange', event => {
+  //     setRoute(parseRoute(window.location.hash));
+  //   });
+  // });
+
+  // const renderPage = () => {
+  //   if (route.path === 'page1') {
+  //     return <Home />;
+  //   }
+  //   if (route.path === 'page2') {
+  //     return <CurrentDay />;
+  //   }
+  // };
+
+  return (
+    <>
+      <Header />
+      <Week />
+    </>
+  );
+};
+
+export default App;
