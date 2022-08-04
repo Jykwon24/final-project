915456792368b51c70295f3c6f9dfe9041a21f37
Jeff Kwon <jykwon007@gmail.com>
2022-08-04 04:03:17 +0000

    fixed workout list on selected day w/ dropdown.

--- client/pages/planner/planner.jsx
+++ client/pages/planner/planner.jsx
@@ -4,8 +4,7 @@ import Redirect from '../../components/redirect';
 
 export default function Planner(props) {
 
-  const { user, route, userList } = useContext(AppContext);
-  // console.log('planner page', user);
+  const { user, route, userList, day } = useContext(AppContext);
 
   if (!user) {
     return <Redirect to='sign-up' />;
@@ -17,17 +16,37 @@ export default function Planner(props) {
     );
   }
 
-  // console.log(userList);
-  // console.log(route);
-  // const selectedDayList = [];
+  // console.log('user list in side planner comp:', userList);
+  // console.log('user in planner:', user);
+  // console.log('day:', day);
+  // console.log('current route:', route);
+  const selectedDayList = userList.filter(workout => workout.date === day);
+
   return (
-    userList.map((element, index) => {
+    selectedDayList.map((element, index) => {
       return (
-      <div key={index}>{element.name}</div>
+        <div key={index} className='accordion-item card'>
+          <div className='card-header d-flex justify-content-center text-bg-light p-2' id={`flush-heading${index}`}>
+            <button className='collapsed dg-background' data-bs-toggle='collapse' data-bs-target={`#flush-collapse${index}`} aria-expanded='true' aria-controls={`flush-collapse${index}`}>
+              <h3 className='text-white'>
+                {element.name}
+              </h3>
+            </button>
+          </div>
+          <div id={`flush-collapse${index}`} className='collapse' aria-labelledby={`flush-heading${index}`} data-parent='#accordionFlush'>
+            {element.details}
+          </div>
+        </div>
       );
 
     })
   );
+
+  // const WorkoutDetails = props => {
+  //   const selectedDayList = userList.filter(workout => workout.date === day);
+  //   selectedDayList.map()
+
+  // };
 
   // return (
   //   <>
