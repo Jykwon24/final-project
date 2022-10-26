e685ba2cab0ced0c65b726891489607feff795ce
Jeff Kwon <jykwon007@gmail.com>
2022-10-26 20:10:58 +0000

    remove tick arrows on nav bar.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -27,23 +27,18 @@ export default function Navbar(props) {
         <div className='nav-bar dg-background d-flex justify-content-between align-items-center ps-3 pe-3'>
           <div className='nav-bar-items'>
             <i className='fa-solid fa-calendar fs-3 text-white' onClick={plannerView}></i>
-            <i className="fa-solid fa-caret-up fs-3 text-white nav-bar-active"></i>
           </div>
           <a className='text-white' href='#stopwatch'>
             <i className='fa-solid fa-stopwatch fs-3' onClick={() => setTarget(defaultTarget)}></i>
-            <i className="fa-solid fa-caret-up"></i>
           </a>
           <a className='text-white' href='#calories'>
             <i className='fa-solid fa-bowl-food fs-3' onClick={() => setTarget(defaultTarget)}></i>
-            <i className="fa-solid fa-caret-up"></i>
           </a>
           <a className='text-white' href='#default-list'>
             <i className='fa-solid fa-circle-plus fs-3' onClick={() => setTarget(defaultTarget)}></i>
-            <i className="fa-solid fa-caret-up"></i>
           </a>
           <a className='text-white' href='#custom-workout'>
             <i className='fa-solid fa-pen fs-3' onClick={defaultCustomView}></i>
-            <i className="fa-solid fa-caret-up"></i>
           </a>
         </div>
       </nav>
